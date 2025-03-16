27cddd7e16dbbfad03686420cd2fce5121803d32
Alex Yankov <yankov.alex@gmail.com>
2025-03-16 11:53:34 -0400

    more tests

--- src/parse.rs
+++ src/parse.rs
@@ -1348,4 +1348,74 @@ include $(MAKE_INCLUDE)
         assert_eq!(parsed.errors, Vec::<String>::new());
 
     }
+
+    #[test]
+    fn test_parse_sphinx_makefile() {
+        const SMALL: &str = r#"
+# Minimal makefile for Sphinx documentation
+#
+
+# You can set these variables from the command line.
+SPHINXOPTS    =
+SPHINXBUILD   = sphinx-build
+SOURCEDIR     = docs
+BUILDDIR      = build
+
+# Put it first so that "make" without argument is like "make help".
+help:
+	@$(SPHINXBUILD) -M help "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+
+.PHONY: help Makefile
+
+# Catch-all target: route all unknown targets to Sphinx using the new
+# "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
+%: Makefile
+	@$(SPHINXBUILD) -M $@ "$(SOURCEDIR)" "$(BUILDDIR)" $(SPHINXOPTS) $(O)
+"#;
+        let parsed = parse(SMALL);
+        assert_eq!(parsed.errors, Vec::<String>::new());
+    }
+
+    #[test]
+    fn test_parse_ohmyzsh_makefile() {
+        const SMALL: &str = r#"
+NAME=zsh-navigation-tools
+
+INSTALL?=install -c
+PREFIX?=/usr/local
+SHARE_DIR?=$(DESTDIR)$(PREFIX)/share/$(NAME)
+DOC_DIR?=$(DESTDIR)$(PREFIX)/share/doc/$(NAME)
+
+all:
+
+install:
+	$(INSTALL) -d $(SHARE_DIR)
+	$(INSTALL) -d $(SHARE_DIR)/.config
+	$(INSTALL) -d $(SHARE_DIR)/.config/znt
+	$(INSTALL) -d $(DOC_DIR)
+	cp zsh-navigation-tools.plugin.zsh _n-kill doc/znt-tmux.zsh $(SHARE_DIR)
+	cp README.md NEWS LICENSE doc/img/n-history2.png $(DOC_DIR)
+	if [ x"true" = x"`git rev-parse --is-inside-work-tree 2>/dev/null`" ]; then \
+		git rev-parse HEAD; \
+	else \
+		cat .revision-hash; \
+	fi > $(SHARE_DIR)/.revision-hash
+	:
+	for fname in n-*; do cp "$$fname" $(SHARE_DIR); done; \
+	for fname in znt-*; do cp "$$fname" $(SHARE_DIR); done; \
+	for fname in .config/znt/n-*; do cp "$$fname" $(SHARE_DIR)/.config/znt; done;
+
+uninstall:
+	rm -f $(SHARE_DIR)/.revision-hash $(SHARE_DIR)/_* $(SHARE_DIR)/zsh-* $(SHARE_DIR)/n-* $(SHARE_DIR)/znt-* $(SHARE_DIR)/.config/znt/n-*
+	[ -d $(SHARE_DIR)/.config/znt ] && rmdir $(SHARE_DIR)/.config/znt || true
+	[ -d $(SHARE_DIR)/.config ] && rmdir $(SHARE_DIR)/.config || true
+	[ -d $(SHARE_DIR) ] && rmdir $(SHARE_DIR) || true
+	rm -f $(DOC_DIR)/README.md $(DOC_DIR)/LICENSE $(DOC_DIR)/n-history2.png 
+	[ -d $(DOC_DIR) ] && rmdir $(DOC_DIR) || true
+
+.PHONY: all install uninstall
+"#;
+        let parsed = parse(SMALL);
+        assert_eq!(parsed.errors, Vec::<String>::new());
+    }
 }
